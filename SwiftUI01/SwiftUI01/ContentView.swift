20c9323e32f86a7a0a2787ec501aa6eb19428ddc
Pawan Kumar <pawan.kumar@appinventiv.com>
2019-06-17 00:07:04 +0530

    Worked on VStack View Swift

--- SwiftUI01/SwiftUI01/ContentView.swift
+++ SwiftUI01/SwiftUI01/ContentView.swift
@@ -18,11 +18,43 @@ import SwiftUI
 // some is the new  keyword of Opaque return type.
 struct ContentView : View {
     
+    
+    static let taskDateFormat: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        return formatter
+    }()
+    
+    var dueDate = Date()
+    
+    
     // here returning some view means that the body property returns something that confirms the view protocol. we can't return many things or  forget to return anything at all the swift compiler will refuse to build out code to be clear your view board must always return exactly one child view fourth.
     var body: some View {
-        Text("Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World ")
-        .truncationMode(.middle)
+//        Text("Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World ")
+//        .truncationMode(.middle)
+//        .font(.largeTitle)
+//        .multilineTextAlignment(.center)
+//        .foregroundColor(Color.red)
+//        .background(Color.yellow)
+        
+//        Text("Task due date: \(dueDate, formatter: Self.taskDateFormat)")
+//        .multilineTextAlignment(.center)
+//        .lineSpacing(20)
+//        .lineLimit(3)
+        
+        
+        VStack(spacing: 50) {
+            Image(systemName: "cloud.heavyrain.fill")
+                .font(.title)
+                .foregroundColor(.red)
+            Divider()
+            Text("Hello")
+        }
+       
+        
     }
+    
+   
     
 }
 
--- SwiftUI01/SwiftUI01/README.md
+++ SwiftUI01/SwiftUI01/README.md
@@ -2,3 +2,6 @@
 
 # 16 june 2019
 1: Text is equivalent to UILabel in UIKIT
+
+2: Text View with fonts ,colors 
+      -- Direct Type
